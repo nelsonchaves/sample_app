96effd7b838e2971f8a4672a689aa386960d60c7
Nelson Chaves <nelson@nelsonchaves.com>
2020-03-17 22:38:51 +0000

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ before_action :admin_user,     only: :destroy
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def new
@@ -55,22 +56,13 @@ before_action :admin_user,     only: :destroy
 
     # Before filters
 
-    # Confirms a logged-in user.
-    def logged_in_user
-      unless logged_in?
-      store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-    end
-
     # Confirms the correct user.
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
 
-# Confirms an admin user.
+    # Confirms an admin user.
     def admin_user
       redirect_to(root_url) unless current_user.admin?
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,6 @@ User.create!(name:  "Nelson Chaves",
              admin: true,
              activated:     true,
              activated_at:  Time.zone.now)
-             
 # Generate a bunch of additional users.
 99.times do |n|
   name  = Faker::Name.name
@@ -18,4 +17,11 @@ User.create!(name:  "Nelson Chaves",
               password_confirmation: password,
               activated: true,
               activated_at: Time.zone.now)
+end
+
+# Generate microposts for a subset of users.
+users = User.order(:created_at).take(6)
+50.times do
+  content = Faker::Lorem.sentence(word_count: 5)
+  users.each { |user| user.microposts.create!(content: content) }
 end
